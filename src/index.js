f5dff4198d8a359a6b5e5764606d7a2ca8f294cd
MGamal <miada.gamal@gmail.com>
2021-05-25 15:21:31 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import middleware from './middleware'
+
 import './index.css';
 import App from './components/App';
-import reducers from './reducers'
+import reducers from './reducers';
 
-const store=createStore(reducers);
+const store=createStore(reducers, middleware);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
