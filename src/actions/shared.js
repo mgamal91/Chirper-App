2963b2da32cd442097836fdf510a8d70fb8ad555
MGamal <miada.gamal@gmail.com>
2021-05-25 13:27:36 +0200

    done authed actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,23 @@
 import { getInitialData } from "../utils/api";
-import {recieveUsers} from './users';
-import {recieveTweets} from './tweets'
+import { recieveUsers } from "./users";
+import { recieveTweets } from "./tweets";
+import { setAuthedUser } from "./authedUser";
+
+//Authed users to pass to action(authedUser.js)
+const AUTHED_ID = "sarah_edo";
+//Authed users
+
 //async request
 export default handleInitialData = () => {
   return (dispatch) => {
-      //call the API and pass both actions the data they need 
-      //recieveUsers with users
-      //recieveTweets with tweets
-      return getInitialData().then(({users, tweets})=>{
-          dispatch(recieveUsers(users));
-          dispatch(recieveTweets(tweets))
-
-      })
+    //call the API and pass both actions the data they need
+    //recieveUsers with users
+    //recieveTweets with tweets
+    //the authed is passed manually here not from api hence const above
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(recieveUsers(users));
+      dispatch(recieveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
   };
 };
