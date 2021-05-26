ef9de128f7477e2ff0a94783ad3d95b9daaa5e2c
MGamal <miada.gamal@gmail.com>
2021-05-26 12:54:23 +0200

    udemy

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+//import tweets from "../reducers/tweetsReducer";
 export class Dashboard extends Component {
   render() {
-    console.log(this.props.tweetIds);
+    const {tweetIds}=this.props;
+    console.log(tweetIds);
     return (
       <div>
         <h3 className="center">Your Timeline</h3>
         <ul className="dashboard-list">
-          {this.props.tweetIds.map((tweet) => (
+          {tweetIds.map((tweet) => (
             <li key={tweet}>
               <div>Tweet ID:{tweet}</div>
             </li>
@@ -18,13 +19,22 @@ export class Dashboard extends Component {
     );
   }
 }
-const mapStateToProps = ({ tweets }) => {
+/* const mapStateToProps = ({ tweets }) => {
   //Object.keys() returns an array whose elements are strings corresponding to the enumerable properties found directly upon object
   //sort from newsest to oldest
   return {
     tweetIds: Object.keys(tweets).sort(
       (a, b) => tweets[b].timestamp - tweets[a].timestamp
     ),
+  };
+}; */
+const mapStateToProps = (state) => {
+  //Object.keys() returns an array whose elements are strings corresponding to the enumerable properties found directly upon object
+  //sort from newsest to oldest
+   console.log('meme',state);
+   return {
+    tweets:state.tweets,
+    tweetIds:Object.keys(state.tweets).sort((a,b)=>state.tweets[b].timestamp - state.tweets[a].timestamp)
   };
 };
 export default connect(mapStateToProps)(Dashboard);
